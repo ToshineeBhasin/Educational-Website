c4ed331d1c602b3d9547896abf5cf106dfb99bf8
Toshinee Bhasin <65332593+ToshineeBhasin@users.noreply.github.com>
2021-01-05 23:31:20 +0530

    website using html, css, javascript

---
+++ Educational Website Design/website/resp.js
@@ -0,0 +1,11 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+navlist=document.querySelector('.nav-list')
+rightnav=document.querySelector('.rightnav')
+
+
+burger.addEventListener('click',()=>{
+    rightnav.classList.toggle('v-class');
+    navlist.classList.toggle('v-class');
+    navbar.classList.toggle('h-nav');
+})
